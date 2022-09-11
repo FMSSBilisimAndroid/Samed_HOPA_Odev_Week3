b982da823643a1a471ef74b62905b0d219cc160d
SamedHOPA <samethopa@gmail.com>
2022-09-11 13:53:30 +0300

    Odev_2 Version 1.2

--- Odev2/app/src/main/java/com/example/odev2/MainActivity.kt
+++ Odev2/app/src/main/java/com/example/odev2/MainActivity.kt
@@ -2,10 +2,8 @@ package com.example.odev2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.odev2.databinding.FragmentMainBinding
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
